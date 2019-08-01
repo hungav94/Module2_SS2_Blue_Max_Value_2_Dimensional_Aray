5df3349224a950226e3ddaca3586e8d90a607faf
hungavcodegym <anhung94@gmail.com>
2019-08-01 11:48:00 +0700

    update

--- src/MaxValue2DimensionalArray.java
+++ src/MaxValue2DimensionalArray.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MaxValue2DimensionalArray {
 
     public static int maxValue(int arr[][]) {
@@ -12,13 +14,27 @@ public class MaxValue2DimensionalArray {
         return max;
     }
 
+    public static int[][] inputArray4x4(int[][] arr) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("Input interger value " + i + " " + j + ": ");
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
-        int arr[][] = {
-                {11, 2, 3, 4},
-                {5, 6, 7, 8},
-                {2, 3, 4, 5},
-                {6, 7, 8, 9}
-        };
+//        int arr[][] = {
+//                {11, 2, 3, 4},
+//                {5, 6, 7, 8},
+//                {2, 3, 4, 5},
+//                {6, 7, 8, 9}
+//        };
+        int[][] arr = new int[4][4];
+        arr = inputArray4x4(arr);
+
         for (int i = 0; i < arr.length; i++) {
             System.out.println("");
             for (int j = 0; j < arr[i].length; j++) {
